3c52ba276020ab57450f2a74dc77fe47e84750ab
Christopher Dancy <chris.dancy@decentlabs.io>
2022-01-20 14:09:13 -0500

    governance and timelockcontroller

---
+++ contracts/TimelockController.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract MyTimelockController is TimelockController{
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    )
+        TimelockController(minDelay, proposers, executors)
+    {}
+}
