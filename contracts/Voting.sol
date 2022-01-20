d414ccc0c6935043e19fe301812e1e814b864071
Christopher Dancy <chris.dancy@decentlabs.io>
2022-01-20 12:47:45 -0500

    added timelock controller to handle execution of transactions

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,8 +2,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
