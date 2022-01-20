5cdd8f823dcbb96fee84a6f09ab347c23cf1191c
Christopher Dancy <chris.dancy@decentlabs.io>
2022-01-20 16:59:29 -0500

    upgradeability hardhat plugin and governance contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
+import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
