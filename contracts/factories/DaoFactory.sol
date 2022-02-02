75c4fc42a6cc093b3885af83c8f1514dc40cb048
tbwebb22 <tbwebb22@gmail.com>
2022-02-02 11:04:40 -0500

    Add Natspec documentation to DaoFactory

--- contracts/factories/DaoFactory.sol
+++ contracts/factories/DaoFactory.sol
@@ -1,5 +1,4 @@
 //SPDX-License-Identifier: Unlicense
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
@@ -7,6 +6,7 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
 import "../MyGovernor.sol";
 import "./TokenFactory.sol";
 
+/// @notice A contract for creating new DAOs 
 contract DaoFactory {
     struct CreateDaoParameters {
         address governanceImplementation; 
@@ -58,6 +58,11 @@ contract DaoFactory {
         address newImplementation
     );
 
+    /// @notice Creates a new DAO and an ERC-20 token that supports voting
+    /// @param createDaoAndTokenParameters Struct of all DAO and token creation parameters
+    /// @return The address of the created voting token contract
+    /// @return The address of the deployed TimelockController contract
+    /// @return The address of the proxy deployed for the created DAO
     function createDaoAndToken(
         CreateDaoAndTokenParameters calldata createDaoAndTokenParameters
     )
@@ -97,6 +102,12 @@ contract DaoFactory {
         return (votingToken, timelockController, proxyAddress);
     }
 
+    /// @notice Creates a new DAO and wraps an existing ERC-20 token 
+    /// @notice with a new governance token that supports voting
+    /// @param createDaoWrapTokenParameters Struct of all DAO and wrapped token creation parameters
+    /// @return The address of the created voting token contract
+    /// @return The address of the deployed TimelockController contract
+    /// @return The address of the proxy deployed for the created DAO
     function createDaoWrapToken(
         CreateDaoWrapTokenParameters calldata createDaoWrapTokenParameters
     )
@@ -133,6 +144,11 @@ contract DaoFactory {
         return (wrappedTokenAddress, timelockController, proxyAddress);
     }
 
+    /// @notice Creates a new DAO with an existing ERC-20 token that supports voting
+    /// @param createDaoBringTokenParameters Struct of all DAO and existing voting token parameters
+    /// @return The address of the voting token contract
+    /// @return The address of the deployed TimelockController contract
+    /// @return The address of the proxy deployed for the created DAO
     function createDaoBringToken(
         CreateDaoBringTokenParameters calldata createDaoBringTokenParameters
     )
@@ -163,6 +179,16 @@ contract DaoFactory {
         return (createDaoBringTokenParameters.tokenAddress, timelockController, proxyAddress);
     }
 
+    /// @dev Creates a new DAO by deploying a new instance of MyGovernor
+    /// @param governanceImplementation The address of the MyGovernor implementation contract
+    /// @param timelockController The address of the TimelockController created for the DAO
+    /// @param daoName The name of the DAO
+    /// @param initialVotingDelay The delay in blocks between when a proposal is submitted and voting begins
+    /// @param initialVotingPeriod The delay in blocks between when a proposal is submitted and voting ends
+    /// @param initialProposalThreshold The number of votes required for a voter to be a proposer
+    /// @param initialQuorumNumeratorValue The numerator for the quorum fraction - the number of votes required
+    /// @param initialQuorumNumeratorValue for a proposal to be successful as a fraction of total supply
+    /// @return The address of the proxy contract deployed for the created 
     function _createDao(
         address governanceImplementation,
         address votingToken,
@@ -201,6 +227,10 @@ contract DaoFactory {
         return proxyAddress;
     }
 
+    /// @dev Deploys a TimelockController contract for the new DAO
+    /// @param proposers Array of addresses that can create proposals
+    /// @param executors Array of addresses that can execute proposals
+    /// @return The address of the deployed TimelockController contract
     function _createTimelock(
         uint256 minDelay,
         address[] memory proposers,
@@ -212,6 +242,10 @@ contract DaoFactory {
         return timelockController;
     }
 
+    /// @dev Configures the timelock controller to give the proxy address 
+    /// @dev proposer and executor roles
+    /// @param _timelock The address of the TimelockController contract
+    /// @param _proxyAddress The address of the MyGovernor proxy
     function _configTimelock(address _timelock, address _proxyAddress) private {
         bytes32 PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
         bytes32 EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
