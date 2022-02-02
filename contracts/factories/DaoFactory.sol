84a4483b90aeec7a5650361407a7dcf08236ba57
tbwebb22 <tbwebb22@gmail.com>
2022-02-02 09:43:55 -0500

    Remove unused import

--- contracts/factories/DaoFactory.sol
+++ contracts/factories/DaoFactory.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/governance/TimelockController.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "../MyGovernor.sol";
 import "./TokenFactory.sol";
 
