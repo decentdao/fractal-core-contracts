428f80bee1fb31a90f2b2e13de08ac8d544977da
Adam Gall <adam@decentlabs.io>
2022-02-27 15:09:25 -0500

    Revert "Pull the OZ AccessControl code into DAOAccessControl"
    This reverts commit dc67f08bd65313750cdb87b102031fb29ac7cb34.

--- contracts/whiteboard/DAOAccessControl.sol
+++ contracts/whiteboard/DAOAccessControl.sol
@@ -1,21 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 import "./IDAOAccessControl.sol";
 
-contract DAOAccessControl is IDAOAccessControl, ERC165, Initializable {
-    struct RoleData {
-        mapping(address => bool) members;
-        bytes32 adminRole;
-    }
-
-    mapping(bytes32 => RoleData) private _roles;
-
-    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
-
+contract DAOAccessControl is
+    IDAOAccessControl,
+    ERC165,
+    Initializable,
+    AccessControl
+{
     function initialize(
         address dao,
         address[] memory executors,
@@ -51,92 +48,26 @@ contract DAOAccessControl is IDAOAccessControl, ERC165, Initializable {
         }
     }
 
-    modifier onlyRole(bytes32 role) {
-        _checkRole(role, msg.sender);
-        _;
-    }
+    // ///////
+    // // TODO: this is hacky maybe
 
-    function hasRole(bytes32 role, address account)
-        public
-        view
-        virtual
-        override
-        returns (bool)
-    {
-        return _roles[role].members[account];
-    }
+    // function grantRole(bytes32, address) public virtual override {
+    //     revert NoOp();
+    // }
 
-    function _checkRole(bytes32 role, address account) internal view virtual {
-        if (!hasRole(role, account)) {
-            revert MissingRole(account, role);
-        }
-    }
+    // function grantRole(string calldata roleID, address moduleAddress, address account) public {
+    //     bytes32 role = keccak256(abi.encodePacked(block.chainid, moduleAddress, roleID));
+    //     _checkRole(getRoleAdmin(role), _msgSender());
+    //     _grantRole(role, account);
+    // }
 
-    function getRoleAdmin(bytes32 role)
-        public
-        view
-        virtual
-        override
-        returns (bytes32)
-    {
-        return _roles[role].adminRole;
-    }
-
-    function grantRole(bytes32 role, address account)
-        public
-        virtual
-        override
-        onlyRole(getRoleAdmin(role))
-    {
-        _grantRole(role, account);
-    }
-
-    function revokeRole(bytes32 role, address account)
-        public
-        virtual
-        override
-        onlyRole(getRoleAdmin(role))
-    {
-        _revokeRole(role, account);
-    }
-
-    function renounceRole(bytes32 role, address account)
-        public
-        virtual
-        override
-    {
-        if (account != msg.sender) {
-            revert OnlySelfCanRenounce();
-        }
-
-        _revokeRole(role, account);
-    }
-
-    function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal virtual {
-        bytes32 previousAdminRole = getRoleAdmin(role);
-        _roles[role].adminRole = adminRole;
-        emit RoleAdminChanged(role, previousAdminRole, adminRole);
-    }
-
-    function _grantRole(bytes32 role, address account) internal virtual {
-        if (!hasRole(role, account)) {
-            _roles[role].members[account] = true;
-            emit RoleGranted(role, account, msg.sender);
-        }
-    }
-
-    function _revokeRole(bytes32 role, address account) internal virtual {
-        if (hasRole(role, account)) {
-            _roles[role].members[account] = false;
-            emit RoleRevoked(role, account, msg.sender);
-        }
-    }
+    // ///////
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
         virtual
-        override
+        override(ERC165, AccessControl)
         returns (bool)
     {
         return
--- contracts/whiteboard/IDAOAccessControl.sol
+++ contracts/whiteboard/IDAOAccessControl.sol
@@ -1,26 +1,12 @@
-// SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-interface IDAOAccessControl {
-    error MissingRole(address account, bytes32 role);
-    error OnlySelfCanRenounce();
-    error InvalidRoles();
+// TODO: maybe we should use the enumerable version, for frontend purposes
+import "@openzeppelin/contracts/access/IAccessControl.sol";
 
-    event RoleAdminChanged(
-        bytes32 indexed role,
-        bytes32 indexed previousAdminRole,
-        bytes32 indexed newAdminRole
-    );
-    event RoleGranted(
-        bytes32 indexed role,
-        address indexed account,
-        address indexed sender
-    );
-    event RoleRevoked(
-        bytes32 indexed role,
-        address indexed account,
-        address indexed sender
-    );
+interface IDAOAccessControl is IAccessControl {
+    error InvalidRoles();
+    error NoOp();
 
     function initialize(
         address dao,
@@ -29,17 +15,4 @@ interface IDAOAccessControl {
         bytes32[] memory rolesAdmins,
         address[][] memory members
     ) external returns (bytes32 EXECUTE_ROLE);
-
-    function hasRole(bytes32 role, address account)
-        external
-        view
-        returns (bool);
-
-    function getRoleAdmin(bytes32 role) external view returns (bytes32);
-
-    function grantRole(bytes32 role, address account) external;
-
-    function revokeRole(bytes32 role, address account) external;
-
-    function renounceRole(bytes32 role, address account) external;
 }
