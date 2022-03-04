32ed0d121a426c7c2895420786b7d41bd161b4f1
Adam Gall <adam@decentlabs.io>
2022-03-04 09:55:11 -0500

    Make indentation of all contracts in whiteboard have 4 spaces

--- contracts/whiteboard/DAO.sol
+++ contracts/whiteboard/DAO.sol
@@ -9,60 +9,62 @@ import "./IDAOAccessControl.sol";
 import "./IDAO.sol";
 
 contract DAO is IDAO, ERC165, Initializable {
-  IDAOAccessControl public accessControl;
+    IDAOAccessControl public accessControl;
 
-  // Are clones upgradeable
-  function initialize(
-    address accessControlImplementation,
-    address[] memory executors,
-    bytes32[] memory roles,
-    bytes32[] memory rolesAdmins,
-    address[][] memory members
-  ) public initializer {
-    accessControl = IDAOAccessControl(
-      address(
-        new ERC1967Proxy(
-          accessControlImplementation,
-          abi.encodeWithSelector(
-            IDAOAccessControl(payable(address(0))).initialize.selector,
-            address(this),
-            executors,
-            roles,
-            rolesAdmins,
-            members
-          )
-        )
-      )
-    );
-  }
-
-  function execute(
-    address[] calldata targets,
-    uint256[] calldata values,
-    bytes[] calldata calldatas
-  ) public {
-    // Todo: Add authorize function modifier
-
-    string memory errorMessage = "DAO: call reverted without message";
-    unchecked {
-      for (uint256 i = 0; i < targets.length; i++) {
-        (bool success, bytes memory returndata) = targets[i].call{
-          value: values[i]
-        }(calldatas[i]);
-        Address.verifyCallResult(success, returndata, errorMessage);
-      }
+    // Are clones upgradeable
+    function initialize(
+        address accessControlImplementation,
+        address[] memory executors,
+        bytes32[] memory roles,
+        bytes32[] memory rolesAdmins,
+        address[][] memory members
+    ) public initializer {
+        accessControl = IDAOAccessControl(
+            address(
+                new ERC1967Proxy(
+                    accessControlImplementation,
+                    abi.encodeWithSelector(
+                        IDAOAccessControl(payable(address(0)))
+                            .initialize
+                            .selector,
+                        address(this),
+                        executors,
+                        roles,
+                        rolesAdmins,
+                        members
+                    )
+                )
+            )
+        );
     }
-  }
 
-  function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    virtual
-    override
-    returns (bool)
-  {
-    return
-      interfaceId == type(IDAO).interfaceId ||
-      super.supportsInterface(interfaceId);
-  }
+    function execute(
+        address[] calldata targets,
+        uint256[] calldata values,
+        bytes[] calldata calldatas
+    ) public {
+        // Todo: Add authorize function modifier
+
+        string memory errorMessage = "DAO: call reverted without message";
+        unchecked {
+            for (uint256 i = 0; i < targets.length; i++) {
+                (bool success, bytes memory returndata) = targets[i].call{
+                    value: values[i]
+                }(calldatas[i]);
+                Address.verifyCallResult(success, returndata, errorMessage);
+            }
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return
+            interfaceId == type(IDAO).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
 }
--- contracts/whiteboard/DAOFactory.sol
+++ contracts/whiteboard/DAOFactory.sol
@@ -7,42 +7,42 @@ import "./IDAOFactory.sol";
 import "./IDAO.sol";
 
 contract DAOFactory is IDAOFactory, ERC165 {
-  function createDAO(
-    address daoImplementation,
-    address accessControlImplementation,
-    address[] memory executors,
-    bytes32[] memory roles,
-    bytes32[] memory rolesAdmins,
-    address[][] memory members
-  ) external returns (address) {
-    address dao = address(
-      new ERC1967Proxy(
-        daoImplementation,
-        abi.encodeWithSelector(
-          IDAO(payable(address(0))).initialize.selector,
-          accessControlImplementation,
-          executors,
-          roles,
-          rolesAdmins,
-          members
-        )
-      )
-    );
+    function createDAO(
+        address daoImplementation,
+        address accessControlImplementation,
+        address[] memory executors,
+        bytes32[] memory roles,
+        bytes32[] memory rolesAdmins,
+        address[][] memory members
+    ) external returns (address) {
+        address dao = address(
+            new ERC1967Proxy(
+                daoImplementation,
+                abi.encodeWithSelector(
+                    IDAO(payable(address(0))).initialize.selector,
+                    accessControlImplementation,
+                    executors,
+                    roles,
+                    rolesAdmins,
+                    members
+                )
+            )
+        );
 
-    emit DAOCreated(dao);
+        emit DAOCreated(dao);
 
-    return dao;
-  }
+        return dao;
+    }
 
-  function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    virtual
-    override
-    returns (bool)
-  {
-    return
-      interfaceId == type(IDAOFactory).interfaceId ||
-      super.supportsInterface(interfaceId);
-  }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return
+            interfaceId == type(IDAOFactory).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
 }
