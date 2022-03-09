5d09996b9812f1e244a016eac8f237e2796b96a9
tbwebb22 <tbwebb22@gmail.com>
2022-03-09 10:41:32 -0500

    Add Natspec to ModuleBase

--- contracts/ModuleBase.sol
+++ contracts/ModuleBase.sol
@@ -5,14 +5,11 @@ import "./interfaces/IModuleBase.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
+/// @notice An abstract contract to be inherited by module contracts
 abstract contract ModuleBase is IModuleBase, UUPSUpgradeable, ERC165 {
     IAccessControl public accessControl;
 
-    function initialize(address _accessControl) public initializer {
-        accessControl = IAccessControl(_accessControl);
-        __UUPSUpgradeable_init();
-    }
-
+    /// @notice Requires that a function caller has the associated role
     modifier authorized() {
         if (
             !accessControl.actionIsAuthorized(
@@ -26,12 +23,25 @@ abstract contract ModuleBase is IModuleBase, UUPSUpgradeable, ERC165 {
         _;
     }
 
+    /// @notice Function for initializing the contract that can only be called once
+    /// @param _accessControl The address of the access control contract
+    function initialize(address _accessControl) public initializer {
+        accessControl = IAccessControl(_accessControl);
+        __UUPSUpgradeable_init();
+    }
+
+    /// @dev Applies authorized modifier so that an upgrade requires
+    /// @dev the caller to have the correct role
+    /// @param newImplementation The address of the new implementation contract being upgraded to
     function _authorizeUpgrade(address newImplementation)
         internal
         override
         authorized
     {}
 
+    /// @notice Returns whether a given interface ID is supported
+    /// @param interfaceId An interface ID bytes4 as defined by ERC-165
+    /// @return bool Indicates whether the interface is supported
     function supportsInterface(bytes4 interfaceId)
         public
         view
