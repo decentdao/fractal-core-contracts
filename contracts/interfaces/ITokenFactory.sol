380fd3b257486309250d3eb8268193d64cb845bd
tbwebb22 <tbwebb22@gmail.com>
2022-04-05 15:46:34 -0400

    Remove unused module factory interfaces

--- contracts/interfaces/ITokenFactory.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-interface ITokenFactory {
-    /// @notice Deploys a new votes token contract
-    function create(bytes[] calldata data) external returns (address token);
-}
