4461adafa3d3bb05bc38c9b0564cfc28deb1f589
tbwebb22 <tbwebb22@gmail.com>
2022-04-05 15:46:45 -0400

    Remove unused import

--- contracts/interfaces/IMetaFactory.sol
+++ contracts/interfaces/IMetaFactory.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "./IGovernorModule.sol";
 import "./IDAOFactory.sol";
-import "./IGovernorFactory.sol";
 
 interface IMetaFactory {
     event DAOAndModulesCreated(address dao, address accessControl, address[] modules);
