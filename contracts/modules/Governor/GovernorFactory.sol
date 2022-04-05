455319127d66e24d112e6549b4eb69b77b6458b1
tbwebb22 <tbwebb22@gmail.com>
2022-04-05 19:39:53 -0400

    Formatting

--- contracts/modules/Governor/GovernorFactory.sol
+++ contracts/modules/Governor/GovernorFactory.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
 import "../../interfaces/IModuleFactory.sol";
 import "../../interfaces/ITimelockUpgradeable.sol";
 import "../../interfaces/IGovernorModule.sol";
