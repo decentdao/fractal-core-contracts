bbb6c4a93e1bdd189a1ad02a1a330cc0bdc47146
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-17 17:45:10 -0400

    removed deploy gov scripts and rename others

--- deploy/core/004_deploy_governorFactory.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
-import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
-
-const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployNonUpgradeable(hre, "GovernorFactory", []);
-};
-
-export default func;
