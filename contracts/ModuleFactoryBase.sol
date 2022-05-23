743942ef91dd8fbbd1c0fc1c1a2f899198a2ab81
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-23 10:51:53 -0400

    added access control

--- contracts/ModuleFactoryBase.sol
+++ contracts/ModuleFactoryBase.sol
@@ -2,10 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "./interfaces/IModuleFactory.sol";
 
 /// @notice An abstract contract to be inherited by module contracts
-abstract contract ModuleFactoryBase is IModuleFactory, ERC165 {
+abstract contract ModuleFactoryBase is
+    IModuleFactory,
+    ERC165,
+    AccessControl,
+    Initializable
+{
+    bytes32 public constant VERSION_ROLE = keccak256("VERSION_ROLE");
     VersionInfo[] public versionControl;
 
     /// @dev add a new version to update module users
@@ -17,13 +25,20 @@ abstract contract ModuleFactoryBase is IModuleFactory, ERC165 {
         string calldata _frontendURI,
         address _impl
     ) external {
+        if (!hasRole(VERSION_ROLE, msg.sender)) {
+            revert NotAuthorized();
+        }
         versionControl.push(VersionInfo(_semanticVersion, _frontendURI, _impl));
+        emit VersionCreated(_semanticVersion, _frontendURI, _impl);
     }
 
     /// @dev Creates a module
     /// @param data The array of bytes used to create the module
     /// @return address[] Array of the created module addresses
-    function create(bytes[] calldata data) external virtual returns (address[] memory);
+    function create(bytes[] calldata data)
+        external
+        virtual
+        returns (address[] memory);
 
     /// @dev Current version of system
     /// @return currentVersion the current VersionInfo
@@ -42,11 +57,17 @@ abstract contract ModuleFactoryBase is IModuleFactory, ERC165 {
         public
         view
         virtual
-        override(ERC165, IModuleFactory)
+        override(ERC165, IModuleFactory, AccessControl)
         returns (bool)
     {
         return
             interfaceId == type(IModuleFactory).interfaceId ||
             super.supportsInterface(interfaceId);
+    }
+
+    /// @notice Function for initializing the contract that can only be called once
+    function __initFactoryBase() internal onlyInitializing {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(VERSION_ROLE, msg.sender);
     }
 }
