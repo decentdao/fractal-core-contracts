af5c18e761fccd97566ba2cf116e585191abc4c9
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-23 16:32:10 -0400

    deploy access control name change

--- deploy/core/004_deploy_accessControl_implementation.ts
+++ deploy/core/004_deploy_accessControl_implementation.ts
@@ -3,7 +3,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployNonUpgradeable(hre, "AccessControl", []);
+  await deployNonUpgradeable(hre, "AccessControlDAO", []);
 };
 
 export default func;
