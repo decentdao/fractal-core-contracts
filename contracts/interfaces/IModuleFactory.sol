2ab50072934197edabbe4ee6280c79e002d84a49
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-23 10:16:48 -0400

    Interface for module factory base

--- contracts/interfaces/IModuleFactory.sol
+++ contracts/interfaces/IModuleFactory.sol
@@ -3,10 +3,41 @@ pragma solidity ^0.8.0;
 
 /// @dev The interface to be inherited by Fractal module factories
 interface IModuleFactory {
+    struct VersionInfo {
+        string semanticVersion;
+        string frontendURI;
+        address impl;
+    }
+
+    /// @dev add a new version to update module users
+    /// @param _semanticVersion semantic version control
+    /// @param _frontendURI IPFS hash of the static frontend
+    /// @param _impl address of the impl
+    function addVersion(
+        string calldata _semanticVersion,
+        string calldata _frontendURI,
+        address _impl
+    ) external;
+
     /// @dev Creates a module
     /// @param data The array of bytes used to create the module
     /// @return address[] Array of the created module addresses
     function create(
         bytes[] calldata data
     ) external returns (address[] memory);
+
+    /// @dev Current version of system
+    /// @return currentVersion the current VersionInfo
+    function currentVersionInfo()
+        external
+        view
+        returns (VersionInfo memory currentVersion);
+
+    /// @notice Returns whether a given interface ID is supported
+    /// @param interfaceId An interface ID bytes4 as defined by ERC-165
+    /// @return bool Indicates whether the interface is supported
+    function supportsInterface(bytes4 interfaceId)
+        external
+        view
+        returns (bool);
 }
