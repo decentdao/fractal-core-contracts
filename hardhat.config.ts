59c6a3ab9627a320c5d180557b093d8064f49141
tbwebb22 <tbwebb22@gmail.com>
2022-06-22 17:15:51 -0400

    Remove unsupported testnets from hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -145,10 +145,7 @@ const config: HardhatUserConfig = {
     deployer: {
       default: 0,
       mainnet: `privatekey://${process.env.MAINNET_DEPLOYER_PRIVATE_KEY}`,
-      ropsten: `privatekey://${process.env.ROPSTEN_DEPLOYER_PRIVATE_KEY}`,
-      rinkeby: `privatekey://${process.env.RINKEBY_DEPLOYER_PRIVATE_KEY}`,
       goerli: `privatekey://${process.env.GOERLI_DEPLOYER_PRIVATE_KEY}`,
-      kovan: `privatekey://${process.env.KOVAN_DEPLOYER_PRIVATE_KEY}`,
     },
   },
   networks: {
@@ -157,25 +154,10 @@ const config: HardhatUserConfig = {
       url: process.env.MAINNET_PROVIDER,
       accounts: [process.env.MAINNET_DEPLOYER_PRIVATE_KEY || ""],
     },
-    ropsten: {
-      chainId: 3,
-      url: process.env.ROPSTEN_PROVIDER,
-      accounts: [process.env.ROPSTEN_DEPLOYER_PRIVATE_KEY || ""],
-    },
-    rinkeby: {
-      chainId: 4,
-      url: process.env.RINKEBY_PROVIDER,
-      accounts: [process.env.RINKEBY_DEPLOYER_PRIVATE_KEY || ""],
-    },
     goerli: {
       chainId: 5,
       url: process.env.GOERLI_PROVIDER,
       accounts: [process.env.GOERLI_DEPLOYER_PRIVATE_KEY || ""],
-    },
-    kovan: {
-      chainId: 42,
-      url: process.env.KOVAN_PROVIDER,
-      accounts: [process.env.KOVAN_DEPLOYER_PRIVATE_KEY || ""],
     },
   },
   etherscan: {
