b9421c5281922dc2fbb4a01b5c3f6047b0b9c329
tbwebb22 <tbwebb22@gmail.com>
2022-06-23 16:35:41 -0400

    Make ModuleFactoryBase ownable instead of using AccessControl, and add inheritance from ERC165Storage

--- contracts/ModuleFactoryBase.sol
+++ contracts/ModuleFactoryBase.sol
@@ -1,15 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 import "./interfaces/IModuleFactoryBase.sol";
 
 /// @notice An abstract contract to be inherited by module contracts
 abstract contract ModuleFactoryBase is
     IModuleFactoryBase,
-    AccessControl,
-    Initializable
+    Ownable,
+    Initializable,
+    ERC165Storage
 {
     bytes32 public constant VERSION_ROLE = keccak256("VERSION_ROLE");
     VersionInfo[] public versionControl;
@@ -22,10 +24,7 @@ abstract contract ModuleFactoryBase is
         string calldata _semanticVersion,
         string calldata _frontendURI,
         address _impl
-    ) external {
-        if (!hasRole(VERSION_ROLE, msg.sender)) {
-            revert NotAuthorized();
-        }
+    ) external onlyOwner {
         versionControl.push(VersionInfo(_semanticVersion, _frontendURI, _impl));
         emit VersionCreated(_semanticVersion, _frontendURI, _impl);
     }
@@ -38,24 +37,8 @@ abstract contract ModuleFactoryBase is
         virtual
         returns (address[] memory);
 
-    /// @notice Returns whether a given interface ID is supported
-    /// @param interfaceId An interface ID bytes4 as defined by ERC-165
-    /// @return bool Indicates whether the interface is supported
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override
-        returns (bool)
-    {
-        return
-            interfaceId == type(IModuleFactoryBase).interfaceId ||
-            super.supportsInterface(interfaceId);
-    }
-
     /// @notice Function for initializing the contract that can only be called once
     function __initFactoryBase() internal onlyInitializing {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(VERSION_ROLE, msg.sender);
+        _registerInterface(type(IModuleFactoryBase).interfaceId);
     }
 }
